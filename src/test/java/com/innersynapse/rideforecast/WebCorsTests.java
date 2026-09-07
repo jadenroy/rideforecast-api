f946e968666dc6424c612bd196c084134ff00db8
Jaden H <jrhylton105@gmail.com>
2026-09-07 04:13:46 -0500

    Allow requests from rideforecast.app

--- src/test/java/com/innersynapse/rideforecast/WebCorsTests.java
+++ src/test/java/com/innersynapse/rideforecast/WebCorsTests.java
@@ -13,12 +13,22 @@ class WebCorsTests {
     @Autowired WebApplicationContext context;
 
     @Test
-    void allowsQuoteSubmissionFromProductionWeb() throws Exception {
+    void allowsQuoteSubmissionFromBrandedProductionWeb() throws Exception {
+        MockMvcBuilders.webAppContextSetup(context).build()
+            .perform(options("/v1/quotes")
+                .header("Origin", "https://rideforecast.app")
+                .header("Access-Control-Request-Method", "POST")
+                .header("Access-Control-Request-Headers", "content-type"))
+            .andExpect(status().isOk())
+            .andExpect(header().string("Access-Control-Allow-Origin", "https://rideforecast.app"));
+    }
+
+    @Test
+    void keepsInnerSynapseDomainCompatible() throws Exception {
         MockMvcBuilders.webAppContextSetup(context).build()
             .perform(options("/v1/quotes")
                 .header("Origin", "https://rideforecast.innersynapse.com")
-                .header("Access-Control-Request-Method", "POST")
-                .header("Access-Control-Request-Headers", "content-type"))
+                .header("Access-Control-Request-Method", "POST"))
             .andExpect(status().isOk())
             .andExpect(header().string("Access-Control-Allow-Origin", "https://rideforecast.innersynapse.com"));
     }
